8667e3631194a3e1b0404ff0e0433d713f1c8b05
Hong Seongjun <129278491+Junepluson@users.noreply.github.com>
2025-04-17 15:47:16 +0900

    reset new start

--- CPP/D2/test2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(){
-    cout << "hello world this is second world!" << endl;
-    return 0;
-}
---
+++ CPP/inflearn/test/test.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+cout << "hello my name is hong"<< "\n";
+return 0;
+}
